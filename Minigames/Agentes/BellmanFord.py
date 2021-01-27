3e3e6313b4ce03341f3031d85cefed0bea1e21ef
Tonix22 <emiliotonix@gmail.com>
2021-01-27 14:18:10 -0700

    fix on queue setup

--- Minigames/Agentes/BellmanFord.py
+++ Minigames/Agentes/BellmanFord.py
@@ -2,18 +2,36 @@
 from Mesh import Mesh
 from queue import Queue
 
+class SetQueue(Queue):
+    def _init(self):
+        self.queue = set()
+    def _put(self, item):
+        self.queue.add(item)
+    def _get(self):
+        return self.queue.pop()
+    def __contains__(self, item):
+        with self.mutex:
+            return item in self.queue
 
-class BellmanImplicit():
-    def __init__(self):
+class BellmanImplicit(Mesh):
+    def __init__(self,row,col,source,offset):
         #put(item) – Put an item into the queue
         #get() – Remove and return an item from the queue. 
-        self.Open   = Queue.Queue()
+        self.Open   = SetQueue()
         self.Closed = []
+        Mesh.__init__(self,row,col,source,offset)
 
-    def AppendNeightbors(s):
+    def AppendNeightbors(self,s):
         for N in s.Neighbors:
             if N is not None:
                 self.Open.put(N)
+
+    def ExpandNode(self,u):
+        succ = []
+        for N in u.Neighbors:
+            if N is not None:
+                succ.append(N)
+            return succ
     #Input: Start node s, function w, function expand, function goal
     #Output: cheapest path from s to t in T, stored in f(s)
 
@@ -21,57 +39,49 @@ class BellmanImplicit():
 
         self.Open.queue.clear()
         del self.Closed[:] # clear Closed
-        AppendNeightbors(s)
+        self.AppendNeightbors(s)
 
         s.value = 0 #f(s) is the Node.value, h(s) is the height, and it is 0
-        while(not Open.empty()): #while Open is not empty
-            u = Open.get() #pop first element from Open
-            Closed.append(u)  #pass from Open queue to close list
-            succ = ExpandNode(u) #Get succesors of u node 
+        while(not self.Open.empty()): #while Open is not empty
+            u = self.Open.get()   #pop first element from Open
+            self.Closed.append(u)  #pass from Open queue to close list
+            succ = self.ExpandNode(u) #Get succesors of u node 
             for v in succ:
-                Improve(u,v)
-
-    def ExpandNode(self,u):
-        succ = []
-        for N in u.Neighbors:
-            if N is not None:
-                succ.append(N)
-        return succ
+                self.Improve(u,v)
     
-    def w(u,v): 
-        return math.abs(u.cord[0] - v.cord[0]) + math.abs(u.cord[1] - v.cord[1])
-
-    def NeedToRelax(self,u,v):
-        return u.value+w(u,v) < v.value
-        
-    def DoRelaxation(self,u,v):
-        return v.value = u.value + w(u,v)
-        
     #Input: Nodes u and v, number of problem graph node n
     #Side Effetcs Update parent of v, f(v), Open and Closed
-    def Improve(self,u,v,n):
-        if (v in Open):
-            if ( NeedToRelax(u,v) ): #Relaxation
-                if ( length(v.path) >= n-1 ):
+    def Improve(self,u,v):
+        n=10 #TODO figure out what is n
+        if (v in self.Open):
+            if ( self.NeedToRelax(u,v) ): #Relaxation
+                if ( len(v.path) >= n-1 ):
                     return
                 v.parent = u 
-                v.value = u.value + w(u,v) #Update f(v) = f(u)+w(u,v) 
+                v.value = u.value + self.weight(u,v) #Update f(v) = f(u)+w(u,v) 
         
-        elif (v in Closed):
-            if( NeedToRelax(u,v) ):
-                if (length(v.path) >= n-1):
+        elif (v in self.Closed):
+            if( self.NeedToRelax(u,v) ):
+                if (len(v.path) >= n-1):
                     return
                 v.parent = u
-                Closed.remove(v)   #remove v from Closed
-                v.value = u.value + w(u,v) #Update f(v) = f(u) + w(u,v)
-                Open.put(v)     #Enqueue v to Open
-        
+                self.Closed.remove(v)   #remove v from Closed
+                v.value = u.value + self.weight(u,v) #Update f(v) = f(u) + w(u,v)
+                self.Open.put(v)     #Enqueue v to Open
         else:
             v.parent = u
-            v.value = u.value + w(u,v) #Initialize f(v) = f(u) + w(u,v)
+            v.value  = u.value + self.weight(u,v) #Initialize f(v) = f(u) + w(u,v)
             self.Open.put(v) #Enqueue v in Open
 
+    def weight(self,u,v): 
+        return abs(u.cord[0] - v.cord[0]) + abs(u.cord[1] - v.cord[1])
 
+    def NeedToRelax(self,u,v):
+        return (u.value+ self.weight(u,v)) < v.value
+        
+    def DoRelaxation(self,u,v):
+        v.value = u.value + self.weight(u,v)
+        return v.value
 
 
 def main():
