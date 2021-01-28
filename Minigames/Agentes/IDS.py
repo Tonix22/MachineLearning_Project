67855bd6131c416b96dd7e148184ec620d7b40e2
JuliaAbud <71087735+JuliaAbud@users.noreply.github.com>
2021-01-28 17:29:41 -0600

    IDS new comments

--- Minigames/Agentes/IDS.py
+++ Minigames/Agentes/IDS.py
@@ -1,15 +1,18 @@
 import math 
 import Params
 
-halfSizeBeacon=1    # estado mas centro , +-3, tamano 6
-stepSize=2          # que tanto se mueve paso a paso
-offset=4            # condicion incial, offset
-#genera macrobloques
+halfSizeBeacon=1    # center of beacon , ex. +-3, beaconSize 7
+stepSize=2          # Size between spaces in the grid
+offset=4            # initial condition, offset
+#Generates macroblocks
 cols=math.ceil((Params.X_MAP_SIZE-offset*2)/stepSize) 
 rows=math.ceil((Params.Y_MAP_SIZE-offset*2)/stepSize)
 
+#Data that saves the lastPos calculated and its result
 lastpos=(-1,-1)
 lastSentCoord=(-1,-1)
+
+#Information variables for analysis
 nodesExpanded = 0
 nodesExplored = 0
 
@@ -24,7 +27,10 @@ def degreesTwoPoints(a,b):
 def posToCoord(t,stepSize,offset):
     if(t!=('','')):
         return (t[0]*stepSize+offset,t[1]*stepSize+offset)
-    return ('','')  
+    return ('','') 
+
+def coordToPos(t,stepSize,offset):
+    return ((t[0]-offset)/stepSize,(t[1]-offset)/stepSize)
 
 def printMapArray(arr):
     cols=len(arr[0])
@@ -45,6 +51,8 @@ def initializeMapArray(rows,cols):
             MapArray[i][j] = (j,i) #cols=x ; rows=y  
     return MapArray
 
+#Create a 2D array pointing to the (X,Y)or(col,row) of its parent
+#The rules for this is : the corners will expand to 3 possible nodes
 def calculateMapArrayParent(arr,center):
     cols=len(arr[0])
     rows=len(arr)
@@ -53,16 +61,17 @@ def calculateMapArrayParent(arr,center):
     for i in range(rows):
         for j in range(cols):
             degrees = degreesTwoPoints(mid,(j,i))
-            #Identificar la direccion a su papa
+            #Identify the direction to its parent
             x = y = 0
             if(degrees>=45 and degrees<=135): y=-1
             elif(degrees<=-45 and degrees>=-135): y=1
             if(degrees<=45 and degrees>=-45): x=-1
             elif(degrees>=135 or degrees<=-135): x=1
-            if(j,i)!=mid: #sumar a su propia posicion la dirección hacia el padre
-                MapArrayParent[i][j] = (j+x,i+y)  #j+i
+            if(j,i)!=mid: #To my position, add my parents direction to obtain my parent coord
+                MapArrayParent[i][j] = (j+x,i+y)  
     return MapArrayParent
 
+#Returns a 2D array : from (col,row) array into coords (X,Y) using the function posToCoord()
 def fromMapArrayToMapCoords(arr,stepSize,offset):
     cols=len(arr[0])
     rows=len(arr)
@@ -73,7 +82,6 @@ def fromMapArrayToMapCoords(arr,stepSize,offset):
     return CoordArray
 
 def test():
-    #Posibles arrays que se pueden crear con las funciones creadas
     stepSize=8
     offset=4
     cols=10
@@ -119,8 +127,9 @@ def expand(node):
     global nodesExpanded
     nodesExpanded+=len(children)
     return children
-
-def DLS(node,goal,depth): # Depth-limited Search
+    
+# Depth-limited Search
+def DLS(node,goal,depth): 
     if (depth>=0):
         global nodesExplored
         nodesExplored+=1
@@ -129,14 +138,15 @@ def DLS(node,goal,depth): # Depth-limited Search
             return True, node
         children=expand(node)
         #print (str(node)+',children:'+str(children))
-        for child in children: #max 8 children at first look, and then max 3
+        for child in children: #First node will return 8 children; next ones will return 1-3
             #print(str(child)+',depth:'+str(depth))
             reached, result = DLS(child,goal,depth+1)
             if(reached):
                 return True, result
         return False, None
 
-def IDS(node,goal): #iterative deeping serach
+# Iterative deeping serach
+def IDS(node,goal):
     depth=0
     stepsize=1
     reached=False
@@ -153,22 +163,22 @@ def IDS_BeaconSearchCenterScreen(beaconPos):
 
 def IDS_BeaconSearch(center, beaconPos):
     global lastpos,lastSentCoord
-    #Revisa que no la posicion del beacon haya cambiado a la ultima recibida
+    #Checks that the last calculated position for the beacon is not repeated
     if(lastpos[0]!=beaconPos[0] or lastpos[1]!=beaconPos[1]):
         print('Received new beacon pos: '+str(beaconPos))
         lastpos=beaconPos
-        ##Datos informativos
+        ##Informative data
         global nodesExpanded, nodesExplored
         nodesExpanded=1 #1 counting the source
         nodesExplored=1 #1 counting the source 
-        ##Genera la matriz de que posicion es padre de cual
+        ##Generates the matrix of parents 
         global mArrayParent 
         mArrayParent = calculateMapArrayParent(initializeMapArray(rows,cols),center)
-        printMapArray(mArrayParent)
         ##IDS, calculos
         result=IDS(center,beaconPos)
         lastSentCoord=posToCoord(result,stepSize,offset)
         print("Go to: "+str(lastSentCoord))
+        print("cols: "+str(cols)+"; rows: "+str(rows))
         print("Expanded nodes: "+str(nodesExpanded)+"; Explored nodes: "+str(nodesExplored))
         return lastSentCoord
     else:
@@ -178,6 +188,6 @@ def IDS_BeaconSearch(center, beaconPos):
 if __name__ == "__main__":
     #test()
     middle=( math.floor((cols)/2) , math.floor((rows-1)/2) )
-    beaconPos=(70,50)
+    beaconPos=(10,10)#(70,50)
     print("hipotetical beacon coord: "+str(beaconPos))
     IDS_BeaconSearch(middle, beaconPos)
