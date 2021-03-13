b40c7519ba650a7cf8fa7f47789924f68a0fa96f
Tonix22 <emiliotonix@gmail.com>
2021-03-12 20:07:04 -0700

    Explorer

--- Minigames/BayesAgent/Explorer.py
+++ Minigames/BayesAgent/Explorer.py
@@ -4,24 +4,50 @@ from pgmpy.inference import BeliefPropagation
 
 class Explorer():
     def __init__(self):
-        self.G = BayesianModel([('exploredUp', 'walk'), ('exploredDown', 'walk'),
-                   ('exploredLeft', 'walk'), ('exploredRight', 'walk')])
-        self.eUp_cpd = TabularCPD('exploredUp', 2, [[0.5], [0.5]])
-        self.eDown_cpd = TabularCPD('exploredDown', 2, [[0.5], [0.5]])
-        self.eLeft_cpd = TabularCPD('exploredLeft', 2, [[0.5], [0.5]])
-        self.eRight_cpd = TabularCPD('exploredRight', 2, [[0.5], [0.5]])
-        self.walk_cpd = TabularCPD('walk', 4,
+        self.G = BayesianModel([('exploredUp', 'walk'), 
+                                ('exploredDown', 'walk'),
+                                ('exploredLeft', 'walk'), 
+                                ('exploredRight', 'walk')])
+
+
+        self.eUp_cpd    = TabularCPD(variable = 'exploredUp', 
+                                    variable_card = 2, 
+                                    values = [[0.5], [0.5]])
+        self.eDown_cpd  = TabularCPD(variable = 'exploredDown', 
+                                    variable_card = 2, 
+                                    values = [[0.5], [0.5]])
+        self.eLeft_cpd  = TabularCPD(variable = 'exploredLeft', 
+                                    variable_card = 2, 
+                                    values = [[0.5], [0.5]])
+        self.eRight_cpd = TabularCPD(variable = 'exploredRight',
+                                    variable_card = 2, 
+                                    values = [[0.5], [0.5]])
+
+        self.walk_cpd   = TabularCPD(variable = 'walk', 
+                                    variable_card = 4,
         #exploredUp     #                   y                   |                    n
         #exploredDown   #         y                 n           |         y                 n           
         #exploredLeft   #   y         n     |    y         n    |    y         n     |    y         n   
         #exploredRight  # y    n    y    n     y    n   y    n    y    n     y    n   y    n     y    n
-                       [[0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1], #walk up
+               values = [[0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1], #walk up
                         [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1], #walk down
                         [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1], #walk left
                         [0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7]], #walk right
                        evidence=['exploredUp', 'exploredDown', 'exploredLeft', 'exploredRight'],
-                       evidence_card=[2, 2, 2, 2]) 
-        G.add_cpds(self.eUp_cpd, self.eDown_cpd, self.eLeft_cpd, self.eRight_cpd, self.walk_cpd)
-        bp = BeliefPropagation(G)
-        bp.calibrate()
+                       evidence_card=[2, 2, 2, 2])
+
+        self.G.add_cpds(self.eUp_cpd, 
+                        self.eDown_cpd, 
+                        self.eLeft_cpd, 
+                        self.eRight_cpd, 
+                        self.walk_cpd)
+
+        self.G.check_model()
+
+
+        bp  = BeliefPropagation(self.G)
+        res = bp.query(variables=["walk"], evidence={'exploredRight':1})
+        print (res)
+
+var = Explorer()
         
